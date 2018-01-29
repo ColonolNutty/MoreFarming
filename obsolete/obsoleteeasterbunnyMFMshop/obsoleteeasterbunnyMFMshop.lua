362435124c39126ce288073c8488bcb79575097f
ColonolNutty <ColonolNutty@hotmail.com>
2018-01-29 12:31:06 -0700

    Replace the easter bunny shop

--- obsolete/obsoleteeasterbunnyMFMshop/obsoleteeasterbunnyMFMshop.lua
+++ obsolete/obsoleteeasterbunnyMFMshop/obsoleteeasterbunnyMFMshop.lua
@@ -1,8 +1,4 @@
-require "/scripts/debugUtilsCN.lua"
-
 function init()
-  DebugUtilsCN.init();
-  -- DebugUtilsCN.enableDebug();
   storage.easterBunnyDetectionRadius = 200.0;
   storage.currentPosition = object.toAbsolutePosition({0, 0});
   storage.queryPosition = {storage.currentPosition[1], storage.currentPosition[2] + 2};
@@ -11,8 +7,10 @@ function init()
 end
 
 function update(dt)
-  storage.easterBunnyId = findEasterBunny()
-  spawnEasterBunnyIfNotExists();
+  if(storage.easterBunnyId == nil) then
+    storage.easterBunnyId = findEasterBunny()
+  end
+  killDatEasterBunny();
 end
 
 function findEasterBunny()
@@ -20,7 +18,6 @@ function findEasterBunny()
     return storage.easterBunnyId;
   end
   storage.easterBunnyId = nil;
-  DebugUtilsCN.logDebug("Easter bunny not detected, searching");
   local nearbyNpcIds = world.npcQuery(storage.currentPosition, 50, { notAnObject = true });
   if(nearbyNpcIds == nil) then
     return nil;
@@ -28,7 +25,6 @@ function findEasterBunny()
   local foundId = nil;
   for _, entityId in pairs(nearbyNpcIds) do
     if(world.npcType(entityId) == storage.easterBunnyType) then
-      DebugUtilsCN.logDebug("Easter Bunny Found");
       foundId = entityId;
       break
     end
@@ -40,7 +36,6 @@ function spawnEasterBunnyIfNotExists()
   if(storage.easterBunnyId ~= nil and world.entityExists(storage.easterBunnyId)) then
     return;
   end
-  DebugUtilsCN.logDebug("Easter bunny does not exist, spawning");
   storage.easterBunnyId = nil;
   local spawnLocation = {storage.currentPosition[1], storage.currentPosition[2] + 2}
   storage.easterBunnyId = world.spawnNpc(spawnLocation, storage.easterBunnySpecies, storage.easterBunnyType, 1);
@@ -48,13 +43,13 @@ function spawnEasterBunnyIfNotExists()
 end
 
 function die()
+  killDatEasterBunny()
+end
+
+function killDatEasterBunny()
   if(storage.easterBunnyId == nil) then
-    DebugUtilsCN.logDebug("No easter bunny id");
     return
   end
-  DebugUtilsCN.logDebug("Breaking easter bunny");
   world.spawnProjectile("npcdeathprojectileMFM", world.entityPosition(storage.easterBunnyId), world.players()[1]); 
-  if(world.entityExists(storage.easterBunnyId)) then
-    DebugUtilsCN.logDebug("Failed to murder easter bunny");
-  end
+  world.entityExists(storage.easterBunnyId)
 end
